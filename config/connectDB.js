0fa1bcbf6392300ddad92f0459bc2044c9759887
Arthur Okolo <Okoloarthur4@gmail.com>
2024-01-28 23:38:50 +0100

    adding database uri

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/movieDB");
+    await mongoose.connect(process.env.DATABASE_URI);
     console.log("Successfully connected to database");
   } catch (error) {
     console.error(`Database connection failed: ${error}`);
